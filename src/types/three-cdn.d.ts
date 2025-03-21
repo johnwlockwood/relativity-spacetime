bf4903119f8160b88cb06019f81191b5e1c3c4a0
John Lockwood <johnwlockwood@gmail.com>
2025-03-21 12:34:10 -0700

    Migrate Three.js from CDN to ES modules with v0.174.0

--- src/types/three-cdn.d.ts
+++
@@ -1,8 +0,0 @@
-// Type declarations for Three.js CDN
-// This file tells TypeScript about the global THREE object
-
-import * as ThreeNamespace from 'three';
-
-declare global {
-  const THREE: typeof ThreeNamespace;
-}
