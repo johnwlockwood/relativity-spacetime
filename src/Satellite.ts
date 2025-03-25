a837c4e0af9be9d8957f797169e9fc9e560794e9
John Lockwood <johnwlockwood@gmail.com>
2025-03-25 14:34:03 -0700

    add satellites and receiver

--- src/Satellite.ts
+++ src/Satellite.ts
@@ -1,12 +1,27 @@
 import * as THREE from 'three';
 
-export class Satellite extends THREE.Mesh {
+export class Satellite {
     clock: number;
     clockRate: number;
-
-    constructor(geometry: THREE.SphereGeometry, material: THREE.MeshPhongMaterial) {
-        super(geometry, material);
+    model: THREE.Object3D | null;
+    position: THREE.Vector3;
+    
+    constructor() {
         this.clock = 0;
         this.clockRate = 1;
+        this.model = null;
+        this.position = new THREE.Vector3();
+    }
+    
+    setModel(model: THREE.Object3D) {
+        this.model = model;
+        this.position = this.model.position;
+    }
+    
+    setPosition(x: number, y: number, z: number) {
+        this.position.set(x, y, z);
+        if (this.model) {
+            this.model.position.set(x, y, z);
+        }
     }
 }
